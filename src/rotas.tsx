c9746b8f3560a87a59f11bfa3267fb659fbd10f3
moreno <moreno.ferreira11@gmail.com>
2025-03-11 21:53:46 -0300

    .

--- src/rotas.tsx
+++ src/rotas.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import Header from "./compontens/Cabecalho";
-import ListaDeProdutos from "./compontens/ListaProdutos";
-import Rodape from "./compontens/Rodape/index";
-import HeaderRestaurante from "./compontens/HeaderDeRestaurante";
-import Banner from "./compontens/Banner";
-import CardapioRestaurante from "./compontens/CardapioRestaurante";
+import Header from "./components/Cabecalho";
+import ListaDeProdutos from "./components/ListaProdutos";
+import Rodape from "./components/Rodape/index";
+import HeaderRestaurante from "./components/HeaderDeRestaurante";
+import Banner from "./components/Banner";
+import CardapioRestaurante from "./components/CardapioRestaurante";
 
 const Rotas = createBrowserRouter([
   {
