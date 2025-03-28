fae1821fee68cae312165647239fbf0bdd80cafa
moreno <moreno.ferreira11@gmail.com>
2025-03-28 11:40:31 -0300

    .

--- src/components/HeaderDeRestaurante/index.tsx
+++ src/components/HeaderDeRestaurante/index.tsx
@@ -1,5 +1,5 @@
 import { Logo } from "../../styles"
-import { Cabecalho, NomeDoRestaurate, } from "./styles"
+import { Cabecalho, NomeDoRestaurate, Centralizador } from "./styles"
 import logo from '../../assets/logo.png'
 import vetor from '../../assets/Vector.png'
 import React from "react"
@@ -9,9 +9,11 @@ import React from "react"
 const HeaderRestaurante = () => {
   return(
     <Cabecalho style={{backgroundImage: `url(${vetor})`}}>
+      <Centralizador>
       <NomeDoRestaurate>Restaurante</NomeDoRestaurate>
       <Logo src={logo} alt="Efood"/>
       <NomeDoRestaurate>0 produtos(s) no seu carrinho</NomeDoRestaurate>
+      </Centralizador>
     </Cabecalho>
   )
 }
--- src/components/HeaderDeRestaurante/styles.ts
+++ src/components/HeaderDeRestaurante/styles.ts
@@ -3,20 +3,28 @@ import { cores } from "../../styles";
 
 export const Cabecalho = styled.div`
   display: flex;
-  justify-content: space-evenly;
   align-items: center;
+  justify-content: space-between;
   max-width: 100%;
   background-color: ${cores.creme} ;
   color: ${cores.vermelho};
-`
+  `
+export const Centralizador = styled.div`
+display: flex;
+  justify-content: space-between; /* ou space-around */
+  align-items: center;
+  margin: auto auto;
+  max-width: 1200px;
 
+
+`
 
 export const TituloH = styled.h1`
   font-size: 36px;
   text-align: center;
   margin-bottom: 32px;
-  margin: 0 auto;
-`
+  `
 export const NomeDoRestaurate = styled.h2`
   font-size: 18px;
+  margin: 0 180px;
 `
